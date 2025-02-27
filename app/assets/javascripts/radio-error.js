3085ed210c30ca96511cebbde156663ca964fb2d
Archana-Gopi <Archana.GOPINATHAN@education.gov.uk>
2025-02-27 15:34:01 +0000

    Updated content in prototype for R2 User testing

---
+++ app/assets/javascripts/radio-error.js
@@ -0,0 +1,32 @@
+function checkRadio() {
+    var radio1 = document.getElementById('radio1');
+    var radio2 = document.getElementById('radio2');
+
+    if (radio1.checked) {
+        window.location = "../list-view-reviewed.html";  
+    }
+        else if(radio2.checked) 
+        {
+            window.location = "../list-view.html";  
+        }
+     
+     else {
+            window.location = "amended-contract-error.html"
+        }
+    }
+    function checkRadio1() {
+        var radio1 = document.getElementById('radio3');
+        var radio2 = document.getElementById('radio4');
+    
+        if (radio1.checked) {
+            window.location = "../list-view-all.html";  
+        }
+            else if(radio2.checked) 
+            {
+                window.location = "../list-view-signed.html";  
+            }
+         
+         else {
+                window.location = "amended-contract-error.html"
+            }
+        }
